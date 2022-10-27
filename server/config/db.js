a9701be48a19ebc1300b83a6cb20ca821ae23550
Mingyue Guo <mguo28@my.centennialcollege.ca>
2022-10-26 20:21:40 -0400

    first commit

---
+++ server/config/db.js
@@ -0,0 +1,5 @@
+module.exports = {
+  //local MongoDB deployment ->
+  URI: "mongodb+srv://Mia:0406@facultyinfo.ofrsl5f.mongodb.net/?retryWrites=true&w=majority"
+  //URI: "mongodb://127.0.0.1/faculties",
+};
